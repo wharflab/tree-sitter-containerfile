afee5dc9e67f4eb759f1cc2a2a462f7d010b38b1
Konstantin Vyatkin <tino@vtkn.io>
2026-04-27 01:12:57 +0200

    Add COPY heredoc injections by extension

--- queries/injections.scm
+++ queries/injections.scm
@@ -9,3 +9,51 @@
   (heredoc_block) @injection.content)
   (#set! injection.language "bash")
   (#set! injection.include-children))
+
+((copy_instruction
+  (param)*
+  (path
+    (heredoc_marker))
+  .
+  (path) @injection.filename
+  .
+  (heredoc_block) @injection.content)
+  (#match? @injection.filename "\\.[jJ][sS][oO][nN]$")
+  (#set! injection.language "json")
+  (#set! injection.include-children))
+
+((copy_instruction
+  (param)*
+  (path
+    (heredoc_marker))
+  .
+  (path) @injection.filename
+  .
+  (heredoc_block) @injection.content)
+  (#match? @injection.filename "\\.[yY][aA]?[mM][lL]$")
+  (#set! injection.language "yaml")
+  (#set! injection.include-children))
+
+((copy_instruction
+  (param)*
+  (path
+    (heredoc_marker))
+  .
+  (path) @injection.filename
+  .
+  (heredoc_block) @injection.content)
+  (#match? @injection.filename "\\.[tT][oO][mM][lL]$")
+  (#set! injection.language "toml")
+  (#set! injection.include-children))
+
+((copy_instruction
+  (param)*
+  (path
+    (heredoc_marker))
+  .
+  (path) @injection.filename
+  .
+  (heredoc_block) @injection.content)
+  (#match? @injection.filename "\\.[xX][mM][lL]$")
+  (#set! injection.language "xml")
+  (#set! injection.include-children))
